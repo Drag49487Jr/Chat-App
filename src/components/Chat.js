f4720b1ae2951d225f1b12d561c9268fbd171718
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-10-21 17:38:03 -0500

    add io connection

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,9 @@
 import React from 'react';
 
-<<<<<<< HEAD
-const Chat =()=> {
-    return(
-        <div>Chat
-        
-        </div>
-    )
-}
-=======
 const Chat = () => {
     return(
         <div>Chat</div>
     )
 };
->>>>>>> 9bc4475279c36b289754035ca0727c41363eb39c
 
 export default Chat;
