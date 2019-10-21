c85b6ecd92a829920d71d3829ef479a1d2581930
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-10-21 17:53:19 -0500

    add join/chat folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => (
   <Switch>
