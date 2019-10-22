3be2049b6a602a8249a121de8260477ee7da5dc7
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-10-21 20:46:43 -0500

    add message/message component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
+import Message from '../Message/Message';
 
 const Messages = ({messages, name}) => (
-<ScrollToBottom>
-    {Messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+<ScrollToBottom className='messages'>
+    {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
 </ScrollToBottom>
 )
 
